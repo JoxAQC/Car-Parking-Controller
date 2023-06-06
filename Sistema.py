f12de0b20feb1737e82fd1635be09389b374e038
Jatziry <jatzirywong9@gmail.com>
2023-06-06 11:25:24 -0500

    Agregando métodos a las clases.

--- Sistema.py
+++ Sistema.py
@@ -1,3 +1,44 @@
 class Sistema:
-    def __init__(self,estado):
-        self.__estado = estado 
+    def __init__(self, estado):
+        self.__estado = estado
+
+    # Getter para el atributo estado
+    def get_estado(self):
+        return self.__estado
+
+    # Setter para el atributo estado
+    def set_estado(self, estado):
+        self.__estado = estado
+
+    def registrarCliente(self):
+        pass
+
+    def crearEstacionamiento(self):
+        pass
+
+    def validarCliente(self):
+        pass
+
+    def ingresarVehiculo(self):
+        pass
+
+    def liberarVehiculo(self):
+        pass
+
+    def generarReporteDiario(self):
+        pass
+
+    def verReportes(self):
+        pass
+
+    def verEstacionamiento(self):
+        pass
+
+    def validarAdministrador(self):
+        pass
+
+    def validarEspacioEstacionamiento(self):
+        pass
+
+    def liberarEstacionamiento(self):
+        pass
