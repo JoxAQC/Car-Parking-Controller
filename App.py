4d76688a8b3bbabbfd68c8da348579e66cc9910d
JoxAQC <jose.quispe35@unmsm.edu.pe>
2023-06-28 21:32:11 -0500

    Corrigiendo reporte semanal a mensual

--- App.py
+++ App.py
@@ -76,7 +76,7 @@ class App(customtkinter.CTk):
         self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
         self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0, text="R. Diario", command=self.reporteDiario)
         self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1, text="R. Semanal", command=self.reporteSem)
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1, text="R. Mensual", command=self.reporteSem)
         self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
         self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2, text="R. Anual", command=self.reporteAnual)
         self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
